90580581e429838d240884cd1bbf51654cce6e87
Lachlan Jowett <lachlan@jowett.net.au>
2023-08-11 23:27:35 +1000

    init

---
+++ fmalloc.c
@@ -0,0 +1,13 @@
+// - Copyright -------------------------------------------------------------------|
+// Copyright (c) 2023 Lachlan Jowett.                                             |
+// Please refer to the license located in 'LICENSE' for permissions and liability.|
+// -------------------------------------------------------------------------------|
+
+#include "fmalloc.h"
+
+int main(int argc, char** argv) {
+
+    
+
+    return 0;
+}
---
+++ fmalloc.h
@@ -0,0 +1,9 @@
+// - Copyright -------------------------------------------------------------------|
+// Copyright (c) 2023 Lachlan Jowett.                                             |
+// Please refer to the license located in 'LICENSE' for permissions and liability.|
+// -------------------------------------------------------------------------------|
+
+#pragma once
+
+#include <stdint.h>
+#include <stdio.h>
